c83b2740ea599889de28292c19b728465915ad23
Gyeong a Kim <alwayz0121@gmail.com>
2022-07-17 19:18:24 +0900

    first project

---
+++ precache-manifest.7e9eba9d0576e0ab5b034dceac8c213c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d43d712d492722ad4a5",
+    "url": "/static/css/main.90d19522.chunk.css"
+  },
+  {
+    "revision": "6d43d712d492722ad4a5",
+    "url": "/static/js/main.6d43d712.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "199af2c2a0c941a3b7e7",
+    "url": "/static/js/2.199af2c2.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "a6a61b7b0ea4f07902e54cddf296b6ef",
+    "url": "/index.html"
+  }
+];
